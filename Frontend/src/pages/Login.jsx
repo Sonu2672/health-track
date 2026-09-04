7802d6abedf4c77373996d5d6aa9e006f95c885d
Sonu2672 <sonukumar133f@gmail.com>
2026-09-04 20:19:46 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,3 +1,201 @@
+// import "../App.css";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { toast } from "react-toastify";
+// function Login({ setIslogin,setIsadmin }) {
+//   const navigate = useNavigate();
+
+//   const [log, setlog] = useState(true);
+
+//   const [firstname, setfirstname] = useState("");
+//   const [lastname, setlastname] = useState("");
+//   const [email, setemail] = useState("");
+//   const [password, setpassword] = useState("");
+//   const [show, setShow] = useState(false);
+
+
+
+//   // LOGIN
+//   const loginhandler = async (e) => {
+//     e.preventDefault();
+
+//     const response = await fetch(
+//       "https://health-track-2b.onrender.com/api/users/login",
+//       {
+//         method: "POST",
+//         credentials: "include",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ email, password }),
+//       },
+//     );
+
+//     const data = await response.json();
+//     //  toast.(data.message);
+//     console.log(data.message);
+    
+//     if(data.message==="admin")
+//     {
+//       setIsadmin(true);
+//        navigate("/dashboard");
+
+//     }
+//     else
+//     {
+//     if (data.message === "login successfully") {
+//       toast.success("Login Successful 🚀");
+//       setIslogin(true);
+//       navigate("/");
+//     } else {
+//       const err = data.message;
+//       toast.error(err);
+//     }
+//   }
+//   };
+
+  
+//   // SIGNUP
+//   const signuphandler = async (e) => {
+//     e.preventDefault();
+
+//     const response = await fetch(
+//       "https://health-track-2b.onrender.com/api/users/signup",
+//       {
+//         method: "POST",
+//         credentials: "include",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ firstname, lastname,email, password }),
+//       },
+//     );
+
+//     const data = await response.json();
+
+//     console.log(data.message);
+
+//     if (data.message === "signup successfully") {
+//       toast.success("Signup Successful 🚀");
+//       setlog(true);
+//       setfirstname("");
+//       setemail("");
+//       setpassword("");
+//     } else {
+//       const err = data.message;
+//       toast.error(err);
+//     }
+//   };
+
+//   return (
+//     <div className="auth-container">
+//       <div className="auth-card">
+//         <h1>{log ? "Login" : "Sign Up"}</h1>
+//         {/* <p>{data.message}</p> */}
+//         <form
+//           onSubmit={log ? loginhandler : signuphandler}
+//           className="auth-form"
+//         >
+//           {!log && (
+//             <>
+//               {/* <span className="material-icons icon">person</span> */}
+//               <input
+//                 type="text"
+//                 value={firstname}
+//                 placeholder="First Name"
+//                 className="input-field"
+//                 onChange={(e) => setfirstname(e.target.value)}
+//               />
+
+//               <input
+//                 type="text"
+//                 value={lastname}
+//                 placeholder="Last Name"
+//                 className="input-field"
+//                 onChange={(e) => setlastname(e.target.value)}
+//               />
+//             </>
+//           )}
+
+//           {/* <span className="material-icons icon">email</span> */}
+//           <input
+//             type="email"
+//             value={email}
+//             placeholder="Email Address"
+//             className="input-field"
+//             onChange={(e) => setemail(e.target.value)}
+//           />
+
+//           <div className="pwd-box">
+//             <input
+//               type={show ? "text" : "password"}
+//               value={password}
+//               placeholder="Password"
+//               className="input-field"
+//               onChange={(e) => setpassword(e.target.value)}
+//             />
+
+//             <span className="eye" onClick={() => setShow(!show)}>
+//               <span className="material-icons">
+//                 {show ? "visibility" : "visibility_off"}
+//               </span>
+//             </span>
+//           </div>
+
+//           <button type="submit" className="auth-btn">
+//             {log ? "Login" : "Sign Up"}
+//           </button>
+//         </form>
+
+//         <p className="auth-footer">
+//           {log ? (
+//             <>
+//               Don't have an account?{" "}
+//               <span onClick={() => setlog(false)}>Sign Up</span>
+//             </>
+//           ) : (
+//             <>
+//               Already have an account?{" "}
+//               <span onClick={() => setlog(true)}>Login</span>
+//             </>
+//           )}
+       
+//           <button
+//             onClick={() => {
+//               window.location.href =
+//                 "https://health-track-2b.onrender.com/auth/google";
+//             }}
+//           >
+//             Login with Google
+//           </button>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import "../App.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -20,7 +218,7 @@ function Login({ setIslogin,setIsadmin }) {
     e.preventDefault();
 
     const response = await fetch(
-      "https://health-track-2b.onrender.com/api/users/login",
+    "https://health-track-2b.onrender.com/api/users/login",
       {
         method: "POST",
         credentials: "include",
@@ -60,7 +258,7 @@ function Login({ setIslogin,setIsadmin }) {
     e.preventDefault();
 
     const response = await fetch(
-      "https://health-track-2b.onrender.com/api/users/signup",
+      "http://localhost:5000/api/users/signup",
       {
         method: "POST",
         credentials: "include",
@@ -163,7 +361,7 @@ function Login({ setIslogin,setIsadmin }) {
           <button
             onClick={() => {
               window.location.href =
-                "https://health-track-2b.onrender.com/auth/google";
+                "http://localhost:5000/auth/google";
             }}
           >
             Login with Google
