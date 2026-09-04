ad451c15b14860c07a8f28e0ff3ea22a4753b52c
Sonu2672 <sonukumar133f@gmail.com>
2026-09-04 20:08:38 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,145 @@
+// import express from "express";
+// import "./auth/google.js";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import passport from "passport";
+// import jwt from "jsonwebtoken";
+// // import authGoogle from "./auth/google.js"
+
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import connectDB from "./config/Db.js";
+// import healthRoutes from "./route/healthRoutes.js";
+// import userRoutes from "./route/userRoutes.js";
+
+
+// import dns from "dns";
+// dns.setServers(["1.1.1.1" , "8.8.8.8"])
+// dotenv.config();
+
+// const app = express();
+
+// // ---------------- DB CONNECT ----------------
+// connectDB().then(() => {
+//   app.listen(process.env.PORT,"0.0.0.0", () => {
+//     console.log("Server running on port", process.env.PORT);
+//   });
+// });
+
+// // ---------------- MIDDLEWARE ----------------
+// app.use(cors({
+//    origin: "https://health-track-2f.onrender.com",
+//   credentials: true
+// }));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(passport.initialize());
+// // app.use(passport.session());
+// // ---------------- ROUTES ----------------
+// // app.use("/api/note", NoteRoutes);
+
+
+// app.use("/api/users", userRoutes);
+// app.use("/api/health",healthRoutes);
+// // app.use("/api/admin", adminRoutes);
+
+
+// // app.use("/api/user", userRoutes);
+// // app.use("/api/profile",profileRoutes);
+
+
+
+// app.get("/", (req, res) => {
+//   res.send("Backend is running");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/auth/google',
+//     passport.authenticate('google', { scope: ["profile", "email"],}));
+ 
+
+// app.get(
+//   "/auth/google/callback",
+//   passport.authenticate("google", {
+//     session: false,
+//     failureRedirect: "/",
+//   }),
+//   async (req, res) => {
+//     try {
+//       console.log("USER:", req.user);
+
+//       if (!req.user) {
+//         return res.status(401).send("Google auth failed");
+//       }
+          
+
+
+//       const token = jwt.sign(
+//         { 
+//         googleId: req.user.id,
+//         id: req.user._id,
+//         role:req.user.role,
+//         email:req.user.email,
+//         firstname: req.user.firstname,
+//         lastname: req.user.lastname , 
+
+//         },
+//         process.env.JWT_SECRET,
+//         { expiresIn: "7d" }
+//       );
+
+// res.cookie("token", token, {
+//   httpOnly: true,
+//   secure: true,
+//   sameSite: "none",
+// });
+// // if(req.user.role==="admin")
+// // {
+// //   return res.redirect("http://localhost:5173/dashboard");
+// // }
+//       res.redirect("https://health-track-2f.onrender.com/patient");
+
+//     } catch (err) {
+//       console.log("ERROR:", err);
+//       return res.status(500).send("Internal Server Error");
+//     }
+//   }
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import express from "express";
 import "./auth/google.js";
 import dotenv from "dotenv";
@@ -11,7 +153,9 @@ import cors from "cors";
 import connectDB from "./config/Db.js";
 import healthRoutes from "./route/healthRoutes.js";
 import userRoutes from "./route/userRoutes.js";
+import  receiveDeviceData from "./route/healthRoutes.js";
 
+import doctorRoutes from "./route/doctorRoutes.js";
 
 import dns from "dns";
 dns.setServers(["1.1.1.1" , "8.8.8.8"])
@@ -43,6 +187,8 @@ app.use(passport.initialize());
 
 app.use("/api/users", userRoutes);
 app.use("/api/health",healthRoutes);
+app.use("/api/devicedata",receiveDeviceData)
+app.use("/api/doctors", doctorRoutes);
 // app.use("/api/admin", adminRoutes);
 
 
@@ -51,9 +197,7 @@ app.use("/api/health",healthRoutes);
 
 
 
-app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
+
 
 
 
@@ -106,6 +250,7 @@ res.cookie("token", token, {
   secure: true,
   sameSite: "none",
 });
+      
 // if(req.user.role==="admin")
 // {
 //   return res.redirect("http://localhost:5173/dashboard");
