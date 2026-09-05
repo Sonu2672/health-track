04750d5f66d323f43bf03e000d47c33de15c1a7a
Sonu2672 <sonukumar133f@gmail.com>
2026-09-05 13:53:22 +0530

    Update HealthHistory.jsx

--- Frontend/src/pages/HealthHistory.jsx
+++ Frontend/src/pages/HealthHistory.jsx
@@ -654,7 +654,7 @@ function HealthHistory() {
 const getHealthHistory = async () => {
   try {
     const response = await fetch(
-       `https://health-track-2b.onrender.com/api/health/history?days=${days}`
+       `https://health-track-2b.onrender.com/api/health/history?days=${days}`,
       {
         credentials: "include",
       }
