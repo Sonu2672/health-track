6f12951e99f68e4332a3c6061dcea5f259c47ad4
Sonu2672 <sonukumar133f@gmail.com>
2026-09-11 22:23:30 +0530

    Update userController.js

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -43,8 +43,8 @@ export const login = async (req, res) => {
 
 res.cookie("token", token, {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000,
 });
 
@@ -78,8 +78,8 @@ res.cookie("token", token, {
 
 res.cookie("token", token, {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000,
 });
 
@@ -110,8 +110,8 @@ res.cookie("token", token, {
 
 res.cookie("token", token, {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000,
 });
 
