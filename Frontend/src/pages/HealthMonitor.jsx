8b9553b2487280e5c55e8c15bdfee4a874980e3e
Sonu2672 <sonukumar133f@gmail.com>
2026-09-11 22:19:13 +0530

    Update HealthMonitor.jsx

--- Frontend/src/pages/HealthMonitor.jsx
+++ Frontend/src/pages/HealthMonitor.jsx
@@ -99,7 +99,7 @@ const [menuOpen, setMenuOpen] = useState(false);
   const Hdata = async () => {
     try {
       const response = await fetch(
-        "http://localhost:5000/api/health/gethealthdata",
+        "hhttps://health-track-2b.onrender.com/api/health/gethealthdata",
         {
           method: "GET",
           credentials: "include",
