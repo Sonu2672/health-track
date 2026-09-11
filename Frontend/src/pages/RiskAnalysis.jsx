77ceaf6e366e4dbb7a57bb680925f905c2660069
Sonu2672 <sonukumar133f@gmail.com>
2026-09-11 22:38:14 +0530

    Update RiskAnalysis.jsx

--- Frontend/src/pages/RiskAnalysis.jsx
+++ Frontend/src/pages/RiskAnalysis.jsx
@@ -631,7 +631,7 @@ function RiskAnalysis() {
     const Hdata = async () => {
       try {
         const response = await fetch(
-          "http://localhost:5000/api/health/gethealthdata",
+          "https://health-track-2b.onrender.com/api/health/gethealthdata",
           {
             method: "GET",
             credentials: "include",
