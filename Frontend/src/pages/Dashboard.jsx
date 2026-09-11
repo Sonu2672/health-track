1e9fc6b7a8e2a1f1917a5d7b34af9c3944d2158c
Sonu2672 <sonukumar133f@gmail.com>
2026-09-11 23:38:03 +0530

    Update Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1774,7 +1774,7 @@ function Dashboard() {
 
         const response =
           await fetch(
-            "http://localhost:5000/api/health/gethealthdata",
+            "https://health-track-2b.onrender.com/api/health/gethealthdata",
             {
               method: "GET",
 
