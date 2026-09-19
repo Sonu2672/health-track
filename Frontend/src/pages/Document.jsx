9cbbcebc5ad77578643466b7514cab2b3f055c7b
Sonu2672 <sonukumar133f@gmail.com>
2026-09-19 13:44:18 +0530

    Update Document.jsx

--- Frontend/src/pages/Document.jsx
+++ Frontend/src/pages/Document.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 
 
-const Docuemnt=()=> {
+const Document=()=> {
 
   const [file, setFile] = useState(null);
   const [reports, setReports] = useState([]);
