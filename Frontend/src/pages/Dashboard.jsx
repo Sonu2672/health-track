aa56a3df2610fd4b813c5fef3ea8b130c12e9fe0
Sonu2672 <sonukumar133f@gmail.com>
2026-09-19 08:53:46 +0530

    Update Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1975,23 +1975,23 @@ function Dashboard() {
     // AUTO REFRESH EVERY 4 SECONDS
     // ==================================================
 
-    const interval =
-      setInterval(() => {
+    // const interval =
+    //   setInterval(() => {
 
-        Hdata();
+    //     Hdata();
 
-      }, 4000);
+    //   }, 4000);
 
 
-    // ==================================================
-    // CLEANUP
-    // ==================================================
+    // // ==================================================
+    // // CLEANUP
+    // // ==================================================
 
-    return () => {
+    // return () => {
 
-      clearInterval(
-        interval
-      );
+    //   clearInterval(
+    //     interval
+    //   );
 
       if (
         "speechSynthesis" in window
