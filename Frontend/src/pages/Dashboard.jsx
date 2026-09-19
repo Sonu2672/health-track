4ad2f302357b5473b2500475678ee4be3ee929d2
Sonu2672 <sonukumar133f@gmail.com>
2026-09-19 11:42:46 +0530

    Update Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -213,7 +213,7 @@ function Dashboard() {
       if (playerId) {
         console.log("🔥 Player ID Found:", playerId);
         
-        const response = await fetch("https://healthtrackb.onrender.com/api/devicedata/", {
+        const response = await fetch("https://healthtrackb.onrender.com/api/devicedata/onesignalid", {
           method: "POST",
           credentials: "include",
           headers: {
