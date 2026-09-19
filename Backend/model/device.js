0f61c4ad6b81c18ca1375de913d060d4b8c03cb3
Sonu2672 <sonukumar133f@gmail.com>
2026-09-19 08:47:44 +0530

    Update device.js

--- Backend/model/device.js
+++ Backend/model/device.js
@@ -1,3 +1,4 @@
+
 import mongoose from "mongoose";
 
 const deviceSchema = new mongoose.Schema(
@@ -5,26 +6,58 @@ const deviceSchema = new mongoose.Schema(
     deviceId: {
       type: String,
       required: true,
-      unique:true,
+      unique: true,
       trim: true,
     },
-
-    
     userid: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-
     oneSignalPlayerId: { 
-    type: String, 
-    default: null 
-  },
-
-
+      type: String, 
+      default: null 
+    },
+  }, // 👈 Yahan fields object close hua aur comma laga
   {
-    timestamps: true,
+    timestamps: true, // 👈 Yeh options object alag se hai
   }
 );
 
 export default mongoose.model("device", deviceSchema);
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const deviceSchema = new mongoose.Schema(
+//   {
+//     deviceId: {
+//       type: String,
+//       required: true,
+//       unique:true,
+//       trim: true,
+//     },
+
+    
+//     userid: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+
+//     oneSignalPlayerId: { 
+//     type: String, 
+//     default: null 
+//   },
+
+
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// export default mongoose.model("device", deviceSchema);
