b705ec3fd8d196db8091bcfb2becabe9fe44b66e
Sonu2672 <sonukumar133f@gmail.com>
2026-09-22 21:46:21 +0530

    Update Notification.jsx

--- Frontend/src/pages/Notification.jsx
+++ Frontend/src/pages/Notification.jsx
@@ -1,49 +1,15 @@
-// import React, { useEffect } from "react";
-// import { getToken } from "firebase/messaging";
-// import { messaging } from "../firebase";
-// const appId = import.meta.env.NOTIFICATION_TOKEN_KEY;
-// const Notification = () => {
-//   useEffect(() => {
-//     const getFCMToken = async () => {
-//       try {
-//         // Browser notification permission
-//         const permission = await Notification.requestPermission();
-
-//         if (permission !== "granted") {
-//           console.log("Notification permission denied");
-//           return;
-//         }
-
-//         // Get FCM Token
-//         const token = await getToken(messaging, {
-//           vapidKey: {appId}
-//         });
-
-//         if (token) {
-//           console.log("FCM Token:", token);
-//         } else {
-//           console.log("No FCM token received");
-//         }
-//       } catch (error) {
-//         console.error("FCM Token Error:", error);
-//       }
-//     };
-
-//     getFCMToken();
-//   }, []);
-
-
-import React, { useEffect } from "react";
+import  { useEffect } from "react";
 import { getToken } from "firebase/messaging";
 import { messaging } from "../firebase";
-const appId = import.meta.env.NOTIFICATION_TOKEN_KEY;
+
+const appId = import.meta.env.VITE_NOTIFICATION_TOKEN_KEY;
 
 const Notification = () => {
   useEffect(() => {
     const getFCMToken = async () => {
       try {
-        // Browser notification permission
-        const permission = await window.Notification.requestPermission();
+        const permission =
+          await window.Notification.requestPermission();
 
         console.log("Notification permission:", permission);
 
@@ -52,9 +18,8 @@ const Notification = () => {
           return;
         }
 
-        // Get FCM token
         const token = await getToken(messaging, {
-          vapidKey: {appId},
+          vapidKey: appId,
         });
 
         if (token) {
@@ -75,7 +40,4 @@ const Notification = () => {
 
 export default Notification;
 
-  return null;
-};
 
-export default Notification;
