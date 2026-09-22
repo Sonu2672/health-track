a5f2a16dfdecdcdc0844542cdebdf71139e38cd2
Sonu2672 <sonukumar133f@gmail.com>
2026-09-22 22:20:18 +0530

    Update Notification.jsx

--- Frontend/src/pages/Notification.jsx
+++ Frontend/src/pages/Notification.jsx
@@ -1,43 +1,43 @@
-import  { useEffect } from "react";
-import { getToken } from "firebase/messaging";
-import { messaging } from "../firebase";
+// import  { useEffect } from "react";
+// import { getToken } from "firebase/messaging";
+// import { messaging } from "../firebase";
 
-const appId = import.meta.env.VITE_NOTIFICATION_TOKEN_KEY;
+// const appId = import.meta.env.VITE_NOTIFICATION_TOKEN_KEY;
 
-const Notification = () => {
-  useEffect(() => {
-    const getFCMToken = async () => {
-      try {
-        const permission =
-          await window.Notification.requestPermission();
+// const Notification = () => {
+//   useEffect(() => {
+//     const getFCMToken = async () => {
+//       try {
+//         const permission =
+//           await window.Notification.requestPermission();
 
-        console.log("Notification permission:", permission);
+//         console.log("Notification permission:", permission);
 
-        if (permission !== "granted") {
-          console.log("Notification permission denied");
-          return;
-        }
+//         if (permission !== "granted") {
+//           console.log("Notification permission denied");
+//           return;
+//         }
 
-        const token = await getToken(messaging, {
-          vapidKey: appId,
-        });
+//         const token = await getToken(messaging, {
+//           vapidKey: appId,
+//         });
 
-        if (token) {
-          console.log("FCM Token:", token);
-        } else {
-          console.log("No FCM token received");
-        }
-      } catch (error) {
-        console.error("FCM Token Error:", error);
-      }
-    };
+//         if (token) {
+//           console.log("FCM Token:", token);
+//         } else {
+//           console.log("No FCM token received");
+//         }
+//       } catch (error) {
+//         console.error("FCM Token Error:", error);
+//       }
+//     };
 
-    getFCMToken();
-  }, []);
+//     getFCMToken();
+//   }, []);
 
-  return null;
-};
+//   return null;
+// };
 
-export default Notification;
+// export default Notification;
 
 
