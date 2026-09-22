cb02675700b444cedf23cfe8e5c81d3f3778734d
Sonu2672 <sonukumar133f@gmail.com>
2026-09-22 22:02:14 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import React from "react";
-// import AppRoutes from "./routes/AppRoutes"
-import Notification from "./pages/Notification"
+import AppRoutes from "./routes/AppRoutes"
+// import Notification from "./pages/Notification"
 import "./App.css"
 
 function App() {
@@ -21,8 +21,8 @@ function App() {
   draggable={false}
   theme="dark"
 />
-     {<Notification/>}
-      {/* <AppRoutes/>  */}
+
+       <AppRoutes/>
       </>  
  )
 }
